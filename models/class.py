0356778df0cdb3345593f5ff6beb80c3980e91bb
Alejo Rios <alejoriosiglesias@gmail.com>
2022-04-24 10:57:07 +0100

    app and models done

---
+++ models/class.py
@@ -0,0 +1,6 @@
+class Class:
+    def __init__(self, name, date, duration, id=None):
+        self.name = name
+        self.date = date
+        self.duration = duration
+        self.id = id
---
+++ models/member.py
@@ -0,0 +1,8 @@
+class Member:
+    def __init__(self,name, age, adress, phone_number, id=None):
+        self.name = name
+        self.age = age
+        self. adress = adress
+        self.phone_number = phone_number
+        self.id = id
+    
